7c9ddba8a22b1d9ab299e0bf7f4018a5d6f090e1
AbbasHusseinOps <210235757+AbbasHusseinOps@users.noreply.github.com>
2025-10-20 17:55:27 +0100

    define input variables for DynamoDB module

---
+++ infra/modules/dynamodb/variable.tf
@@ -0,0 +1,14 @@
+variable "table_name" {
+  type = string
+  default = "Abbas-url-app"
+}
+
+variable "pk_name" {
+  type    = string
+  default = "pk"
+}
+
+variable "pk_type" {
+  type    = string
+  default = "S"
+}
