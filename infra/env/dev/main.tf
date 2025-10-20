7608ad695b67845a61fd4cb747fe25daeb4528db
AbbasHusseinOps <210235757+AbbasHusseinOps@users.noreply.github.com>
2025-10-20 10:18:36 +0100

    reference ALB module in dev environment

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -16,3 +16,11 @@ module "sgs" {
 source = "../../modules/sgs"
 vpc_id = module.vpc.vpc_id
 }
+
+module "alb" {
+source = "../../modules/alb"
+vpc_id = module.vpc.vpc_id
+certificate_arn = var.certificate_arn
+alb_sg_id = module.sgs.alb_sg_id
+public_subnet_ids = module.vpc.public_subnet_ids
+}
