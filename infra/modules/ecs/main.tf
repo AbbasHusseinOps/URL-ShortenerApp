1007d26475be3415c0c0c420648dceafb9bcc3cb
AbbasHusseinOps <210235757+AbbasHusseinOps@users.noreply.github.com>
2025-10-20 17:55:27 +0100

    define ECS cluster, task definition, and service resources

---
+++ infra/modules/ecs/main.tf
@@ -0,0 +1,78 @@
+resource "aws_ecs_cluster" "this" {
+  name = var.cluster_name
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = var.log_group_name
+  retention_in_days = var.log_retention
+}
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = var.task_family
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.cpu
+  memory                   = var.memory
+  execution_role_arn       = var.execution_role_arn
+  task_role_arn            = var.task_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.container_name
+      image     = var.container_image
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = var.container_port
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = [
+        {
+          name  = "TABLE_NAME"
+          value = var.db_table_name
+        },
+        {
+          name  = "AWS_REGION"
+          value = var.region
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = var.log_group_name
+          awslogs-region        = var.region
+          awslogs-stream-prefix = var.container_name
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "this" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  deployment_controller { type = "CODE_DEPLOY" }
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = var.security_group_ids
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = var.prod_target_group_arn
+    container_name   = var.container_name
+    container_port   = var.container_port
+  }
+
+  lifecycle { ignore_changes = [task_definition] }
+
+}
