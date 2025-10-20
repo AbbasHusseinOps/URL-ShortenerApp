d151202c18da9ceedf8c27d3da0d774002ee657d
AbbasHusseinOps <210235757+AbbasHusseinOps@users.noreply.github.com>
2025-10-20 09:13:02 +0100

    Add Terraform bootstrap and native S3 backend setup

---
+++ infra/env/dev/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.13.0"
+  backend "s3" {
+    bucket        = "abbas-url-shortener-app"
+    key           = "envs/dev/terraform.tfstate"
+    region        = "eu-west-2"
+    encrypt       = true
+    use_lockfile  = true
+  }
+}
---
+++ infra/state/variable.tf
@@ -0,0 +1,8 @@
+variable "region" {
+    type = string
+    default = "eu-west-2"
+}
+variable "state_bucket_name" {
+  type = string
+  default = "abbas-url-shortener-app"
+}
