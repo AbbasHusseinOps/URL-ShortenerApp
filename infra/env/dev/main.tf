eee5511e39c26811632cec395a0fa2f2e0025c9d
AbbasHusseinOps <210235757+AbbasHusseinOps@users.noreply.github.com>
2025-10-20 11:11:19 +0100

    reference ACM module

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -20,7 +20,7 @@ vpc_id = module.vpc.vpc_id
 module "alb" {
 source = "../../modules/alb"
 vpc_id = module.vpc.vpc_id
-certificate_arn = var.certificate_arn
+certificate_arn = module.acm.certificate_arn
 alb_sg_id = module.sgs.alb_sg_id
 public_subnet_ids = module.vpc.public_subnet_ids
 }
@@ -29,4 +29,9 @@ module "route53" {
 source = "../../modules/route53"
 alb_dns_name = module.alb.alb_dns_name
 alb_zone_id = module.alb.alb_zone_id
+}
+module "acm" {
+source = "../../modules/acm"
+sub_zone_id = module.route53.sub_zone_id
+ns_delegate_record_id = module.route53.ns_delegate_record_id
 }
