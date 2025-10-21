8553edf1dabb6af212891fd7122916c9b0c01e56
AbbasHusseinOps <210235757+AbbasHusseinOps@users.noreply.github.com>
2025-10-21 10:27:27 +0100

    add outputs for CodeDeploy DGs IDs & Names

---
+++ infra/modules/codedeploy/output.tf
@@ -0,0 +1,10 @@
+output "deployment_group_name" {
+  value = aws_codedeploy_deployment_group.ecs.deployment_group_name
+}
+
+output "deployment_group_id" {
+  value = aws_codedeploy_deployment_group.ecs.id
+}
+output "codedeploy_app" {
+  value = aws_codedeploy_app.deployer.id
+}
