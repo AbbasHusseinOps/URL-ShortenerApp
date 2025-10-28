ba09ed9c01d8efea57c97af547ea2db6b435394b
AbbasHusseinOps <210235757+AbbasHusseinOps@users.noreply.github.com>
2025-10-28 10:36:38 +0000

    chore(acm): expose the validated certificate ARN output for use in ALB and CodeDeploy

--- infra/modules/acm/output.tf
+++ infra/modules/acm/output.tf
@@ -1,5 +1,5 @@
 output "certificate_arn"  {
-value = aws_acm_certificate.this.arn 
+value = aws_acm_certificate_validation.this.certificate_arn
 }
 output "validation_fqdns" { 
 value = [for r in aws_route53_record.acm_validation : r.fqdn] 
