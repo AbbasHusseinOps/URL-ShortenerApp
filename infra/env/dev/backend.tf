65b2bd1138e6cebd9665d7ed9cd3791e694acc56
AbbasHusseinOps <210235757+AbbasHusseinOps@users.noreply.github.com>
2025-11-23 11:40:53 +0000

    fix the tfm fmt error

--- infra/env/dev/backend.tf
+++ infra/env/dev/backend.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version =">= 1.13.0"
+  required_version = ">= 1.13.0"
   backend "s3" {
-    bucket        = "abbas-url-shortener-app"
-    key           = "envs/dev/terraform.tfstate"
-    region        = "eu-west-2"
-    encrypt       = true
-    use_lockfile  = true
+    bucket       = "abbas-url-shortener-app"
+    key          = "envs/dev/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
