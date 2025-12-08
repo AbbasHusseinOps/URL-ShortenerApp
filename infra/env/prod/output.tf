28eff995c0803475d46b54b833ea4ea3e6467385
AbbasHusseinOps <210235757+AbbasHusseinOps@users.noreply.github.com>
2025-12-08 11:32:57 +0000

    add the prod env output

---
+++ infra/env/prod/output.tf
@@ -0,0 +1,31 @@
+output "ecs_cluster_name" {
+  value = module.ecs.cluster_name
+}
+
+output "ecs_service_name" {
+  value = module.ecs.service_name
+}
+
+output "ecs_container_name" {
+  value = module.ecs.container_name
+}
+
+output "ecr_repository_name" {
+  value = module.ecr.name
+}
+
+output "codedeploy_application_name" {
+  value = module.codedeploy.application_name
+}
+
+output "codedeploy_deployment_group_name" {
+  value = module.codedeploy.deployment_group_name
+}
+
+output "dynamodb_table_name" {
+  value = module.dynamodb.table_name
+}
+
+output "dynamodb_table_arn" {
+  value = module.dynamodb.table_arn
+}
